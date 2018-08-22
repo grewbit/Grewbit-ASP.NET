641a4434fb35f4712de70be485beb361d756fb27
Samuel <sam.zhen95@gmail.com>
2018-08-22 14:42:33 +0700

    Update AccountController SignUp Post method

--- src/Grewbit/GrewbitWeb/Controllers/AccountController.cs
+++ src/Grewbit/GrewbitWeb/Controllers/AccountController.cs
@@ -5,22 +5,53 @@ using System.Web;
 using System.Web.Mvc;
 using GrewbitWeb.ViewModels;
 using GrewbitShared.Models;
+using GrewbitShared.Security;
+using Microsoft.Owin.Security;
+using System.Threading.Tasks;
 
 namespace GrewbitWeb.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly ApplicationUserManager _userManager;
+        private readonly ApplicationSignInManager _signInManager;
+        private readonly IAuthenticationManager _authenticationManager;
+
+        public AccountController(
+            ApplicationUserManager userManager,
+            ApplicationSignInManager signInManager,
+            IAuthenticationManager authenticationManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _authenticationManager = authenticationManager;
+        }
+
         public ActionResult SignUp()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult SignUp(AccountSignUpViewModel viewModel)
+        public async Task<ActionResult> SignUp(AccountSignUpViewModel viewModel)
         {
             if (ModelState.IsValid)
             {
                 var user = new User { UserName = viewModel.Email, Email = viewModel.Email };
+
+                var result = await _userManager.CreateAsync(user, viewModel.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error);
+                }
             }
             return View(viewModel);
         }
