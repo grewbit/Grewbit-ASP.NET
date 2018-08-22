01b1500899b5320e50ccde126a68d24d2e1e0e37
Samuel <sam.zhen95@gmail.com>
2018-08-22 13:59:52 +0700

    Add support for sign up page

---
+++ src/Grewbit/GrewbitWeb/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GrewbitWeb.ViewModels;
+
+namespace GrewbitWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(AccountSignUpViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
